0e7edaf7037f94acba249a5293564a53c977c769
Sonavin <sonavin.rem@gmail.com>
2024-10-31 14:27:20 +0700

    Add img and update app.vue
    Add 3 components successfully

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
-<script setup></script>
+<script>
+import category from "./components/category_component.vue";
+import button from "./components/button_component.vue";
+import promotion from "./components/promotion_component.vue";
+
+export default {
+  components: {
+    category,
+    button,
+    promotion,
+  },
+  data() {
+    return {
+      category: [],
+      promotion: [],
+      button: [],
+    };
+  },
+};
+</script>
 
 <template>
+  <div id="app">
+    <category> </category>
+    <promotion> </promotion>
+    <button></button>
+  </div>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: black;
+}
+body {
+  background-color: aliceblue;
+}
+</style>
